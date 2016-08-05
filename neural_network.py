256ff87e1d18449618a0ccb549c805aac0123dab
Skittlekiller <ajclackey@gmail.com>
2016-08-04 22:23:15 -0400

    Add files via upload

---
+++ neural_network.py
@@ -0,0 +1,22 @@
+#Neural Network Implementation
+
+#structure of the network
+#single neuron layer with x inputs
+#y neurons  ( may be reduced through trimming)
+#z outputs ( start with binary)
+
+def input_layer(x):
+	#here is the input layer list
+	if x.type = list:
+		neuron_layer(x)
+
+def neuron_layer(x):
+	#define connections
+	#define rng weights for connections
+	
+def output_layer(y):
+	#classify
+	
+def finished():
+	#output a file with the completed network
+	
---
+++ rng_function.py
@@ -0,0 +1,6 @@
+import datetime
+
+def rng_function(x):
+	rng_list = [0,1,2,3,4,5,6,7,8,9]
+	
+
